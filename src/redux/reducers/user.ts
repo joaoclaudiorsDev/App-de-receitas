8e9cbd32404b1da5b1a24e957d5b174b7eded8a9
Vinicius Tibo <viniciustibo.vt@gmail.com>
2023-08-18 18:10:09 -0300

    atualizando a main

---
+++ src/redux/reducers/user.ts
@@ -0,0 +1,23 @@
+import { SAVE_USER } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+type ActionType = {
+  type: string;
+  payload: string;
+};
+
+function user(state = INITIAL_STATE, action: ActionType) {
+  switch (action.type) {
+    case SAVE_USER:
+      return {
+        email: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+export default user;
