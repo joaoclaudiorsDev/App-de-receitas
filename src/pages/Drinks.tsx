49b2bdf8b3e0f6bf81d0ed4d4aee558c229281b9
Eberton <ebertonsouza@unigranrio.br>
2023-08-22 13:15:23 -0300

    renderiza as bebidas pesquisadas

--- src/pages/Drinks.tsx
+++ src/pages/Drinks.tsx
@@ -1,8 +1,26 @@
+import { useSelector } from 'react-redux';
+import { ReduxState } from '../types';
+
 function Drinks() {
+  const recipes = useSelector(
+    (state: ReduxState) => state.recipes.drinkRecipes,
+  );
+
   return (
-    <div>
-      <h1>...</h1>
-    </div>
+    <>
+      {
+        recipes.map((recipe, index) => (
+          <div key={ recipe.idDrink } data-testid={ `${index}-recipe-card` }>
+            <img
+              src={ recipe.strDrinkThumb }
+              alt={ recipe.strDrink }
+              data-testid={ `${index}-card-img` }
+            />
+            <h2 data-testid={ `${index}-card-name` }>{recipe.strDrink}</h2>
+          </div>
+        ))
+      }
+    </>
   );
 }
 
