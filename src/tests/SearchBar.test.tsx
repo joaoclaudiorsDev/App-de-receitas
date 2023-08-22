c542c780270a58c5f84b625ec097dae6d0742283
Eberton <ebertonsouza@unigranrio.br>
2023-08-22 16:13:51 -0300

    finaliza testes de searchBar

--- src/tests/SearchBar.test.tsx
+++ src/tests/SearchBar.test.tsx
@@ -1,5 +1,6 @@
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { vi } from 'vitest';
 import App from '../App';
 import { renderWithRouterAndRedux } from './helpers/RenderWith';
 
@@ -42,6 +43,94 @@ describe('Testing SearchBar component', () => {
     const result = await screen.findAllByRole('heading', { level: 2 });
 
     expect(result).toHaveLength(12);
+  });
+
+  test('Check if SearchBar throw error if has wrong recipe. Meals page', async () => {
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/meals'] });
+
+    vi.spyOn(window, 'alert').mockImplementation(() => {});
+
+    const searchBtn = screen.getByRole('button', { name: /search-icon/i });
+
+    await userEvent.click(searchBtn);
+
+    const searchInput = screen.getByRole('textbox');
+    const ingredientInput = screen.getByRole('radio', { name: /ingrediente/i });
+    const submitButton = screen.getByRole('button', { name: /pesquisar/i });
+
+    await userEvent.type(searchInput, 'anything');
+    await userEvent.click(ingredientInput);
+    await userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(window.alert).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  test('Check if SearchBar throw error if has more then 1 letter to search firstLetter recipe. Meals page', async () => {
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/meals'] });
+
+    vi.spyOn(window, 'alert').mockImplementation(() => {});
+
+    const searchBtn = screen.getByRole('button', { name: /search-icon/i });
+
+    await userEvent.click(searchBtn);
+
+    const searchInput = screen.getByRole('textbox');
+    const ingredientInput = screen.getByRole('radio', { name: /primeira letra/i });
+    const submitButton = screen.getByRole('button', { name: /pesquisar/i });
+
+    await userEvent.type(searchInput, 'anything');
+    await userEvent.click(ingredientInput);
+    await userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(window.alert).toHaveBeenCalled();
+    });
+  });
+
+  test('Check if SearchBar throw error if has wrong recipe. Drinks page', async () => {
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/drinks'] });
+
+    vi.spyOn(window, 'alert').mockImplementation(() => {});
+
+    const searchBtn = screen.getByRole('button', { name: /search-icon/i });
+
+    await userEvent.click(searchBtn);
+
+    const searchInput = screen.getByRole('textbox');
+    const ingredientInput = screen.getByRole('radio', { name: /name/i });
+    const submitButton = screen.getByRole('button', { name: /pesquisar/i });
+
+    await userEvent.type(searchInput, 'anything');
+    await userEvent.click(ingredientInput);
+    await userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(window.alert).toHaveBeenCalled();
+    });
+  });
+
+  test('Check if SearchBar throw error if has more then 1 letter to search firstLetter recipe. Drinks page', async () => {
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/drinks'] });
+
+    vi.spyOn(window, 'alert').mockImplementation(() => {});
+
+    const searchBtn = screen.getByRole('button', { name: /search-icon/i });
+
+    await userEvent.click(searchBtn);
+
+    const searchInput = screen.getByRole('textbox');
+    const ingredientInput = screen.getByRole('radio', { name: /primeira letra/i });
+    const submitButton = screen.getByRole('button', { name: /pesquisar/i });
+
+    await userEvent.type(searchInput, 'anything');
+    await userEvent.click(ingredientInput);
+    await userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(window.alert).toHaveBeenCalled();
+    });
   });
 
   test('Check if SearchBar search food name correctly if only have one recipe when submit. Meals page', async () => {
@@ -104,7 +193,7 @@ describe('Testing SearchBar component', () => {
     expect(result).toHaveLength(12);
   });
 
-  test('Check if SearchBar search food name correctly if only have one recipe when submit. Drinks page', async () => {
+  test('Check if SearchBar search drink name correctly if only have one recipe when submit. Drinks page', async () => {
     renderWithRouterAndRedux(<App />, { initialEntries: ['/drinks'] });
 
     const searchBtn = screen.getByRole('button', { name: /search-icon/i });
@@ -122,5 +211,45 @@ describe('Testing SearchBar component', () => {
     const result = await screen.findByRole('heading', { name: /Pysch Vitamin Light/i, level: 1 });
 
     expect(result).toBeInTheDocument();
+  });
+
+  test('Check if SearchBar search drink correctly if only have one letter when submit. Drinks page', async () => {
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/drinks'] });
+
+    const searchBtn = screen.getByRole('button', { name: /search-icon/i });
+
+    await userEvent.click(searchBtn);
+
+    const searchInput = screen.getByRole('textbox');
+    const nameInput = screen.getByRole('radio', { name: /primeira letra/i });
+    const submitButton = screen.getByRole('button', { name: /pesquisar/i });
+
+    await userEvent.type(searchInput, 'v');
+    await userEvent.click(nameInput);
+    await userEvent.click(submitButton);
+
+    const result = await screen.findAllByRole('heading', { level: 2 });
+
+    expect(result).toHaveLength(12);
+  });
+
+  test('Check if SearchBar search food correctly if only have one letter when submit. Meals page', async () => {
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/meals'] });
+
+    const searchBtn = screen.getByRole('button', { name: /search-icon/i });
+
+    await userEvent.click(searchBtn);
+
+    const searchInput = screen.getByRole('textbox');
+    const nameInput = screen.getByRole('radio', { name: /primeira letra/i });
+    const submitButton = screen.getByRole('button', { name: /pesquisar/i });
+
+    await userEvent.type(searchInput, 'v');
+    await userEvent.click(nameInput);
+    await userEvent.click(submitButton);
+
+    const result = await screen.findAllByRole('heading', { level: 2 });
+
+    expect(result).toHaveLength(7);
   });
 });
