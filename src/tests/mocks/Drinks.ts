f370950527745572e91baa1fdf6c95a4882b4e97
Eberton <ebertonsouza@unigranrio.br>
2023-08-22 23:42:42 -0300

    cria mocks para testes

---
+++ src/tests/mocks/Drinks.ts
@@ -0,0 +1,73 @@
+export const drinkByName = {
+  drinks: [
+    {
+      strDrink: 'Cafe Savoy',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vqwptt1441247711.jpg',
+      idDrink: '14181',
+    },
+  ],
+};
+
+export const drinksMock = {
+  drinks: [
+    {
+      strDrink: 'Baby Eskimo',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/wywrtw1472720227.jpg',
+      idDrink: '15511',
+    },
+    {
+      strDrink: 'Banana Milk Shake',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/rtwwsx1472720307.jpg',
+      idDrink: '12654',
+    },
+    {
+      strDrink: 'Boozy Snickers Milkshake',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/861tzm1504784164.jpg',
+      idDrink: '17221',
+    },
+    {
+      strDrink: 'Butter Baby',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/1xhjk91504783763.jpg',
+      idDrink: '17220',
+    },
+    {
+      strDrink: 'Cafe Savoy',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vqwptt1441247711.jpg',
+      idDrink: '14181',
+    },
+    {
+      strDrink: 'Chocolate Beverage',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/jbqrhv1487603186.jpg',
+      idDrink: '12732',
+    },
+    {
+      strDrink: 'Chocolate Drink',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/q7w4xu1487603180.jpg',
+      idDrink: '12734',
+    },
+    {
+      strDrink: 'Chocolate Milk',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/j6q35t1504889399.jpg',
+      idDrink: '13328',
+    },
+    {
+      strDrink: 'Drinking Chocolate',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/u6jrdf1487603173.jpg',
+      idDrink: '12736',
+    },
+    {
+      strDrink: 'Egg Cream',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/mvis731484430445.jpg',
+      idDrink: '12668',
+    },
+    {
+      strDrink: 'Egg Nog #4',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/wpspsy1468875747.jpg',
+      idDrink: '12910',
+    },
+    {
+      strDrink: 'Frappé',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vqwryq1441245927.jpg',
+      idDrink: '12768',
+    }],
+};
