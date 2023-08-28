77a22e85253c1b1c1a1d908298fa1a5ad27babb8
Vinicius Tibo <viniciustibo.vt@gmail.com>
2023-08-28 14:53:38 -0300

    pronto pro merge

--- src/tests/RecipeDetails.test.tsx
+++ src/tests/RecipeDetails.test.tsx
@@ -1,5 +1,5 @@
 import { vi } from 'vitest';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import { renderWithRouterAndRedux } from './helpers/RenderWith';
