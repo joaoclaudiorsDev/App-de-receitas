29b320652028478d5619a8f7cdac46dacfcefad8
Eberton <ebertonsouza@unigranrio.br>
2023-08-29 06:07:14 -0300

    adiciona checkbox aos ingredientes e doneRecipes

--- src/pages/RecipeInProgress.tsx
+++ src/pages/RecipeInProgress.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { fetchDrinkId, fetchMealId } from '../utils/fetchAPI';
-import { DrinksType, MealType } from '../types';
+import { DrinksType, MealType, DoneRecipeType } from '../types';
 import styles from './RecipeInProgess.module.css';
 import FavoriteButton from '../components/FavoriteButton';
 
@@ -12,8 +12,11 @@ function RecipeInProgess() {
   const [mealRecipe, setMealRecipe] = useState<MealType | undefined>();
   const [drinkRecipe, setDrinkRecipe] = useState<DrinksType>();
   const [ingredients, setIngredients] = useState<string[]>();
+  const [mesures, setMesures] = useState<string[]>();
+  const [isLoaded, setIsLoaded] = useState(true);
   const [done, setDone] = useState({} as any);
   const [allChecked, setAllChecked] = useState(false);
+  const [doneRecipes, setDoneRecipes] = useState<DoneRecipeType[]>([]);
 
   const fetchRecipe = async () => {
     if (pathname.includes('meals')) {
@@ -24,6 +27,11 @@ function RecipeInProgess() {
           .includes('Ingredient') && ingredient[1] !== '' && ingredient[1] !== null
         )).map((ingredient) => ingredient[1]);
       setIngredients(ingredientsList as string[]);
+      const mesuresList = Object.entries(recipeData)
+        .filter((mesure) => (mesure[0]
+          .includes('Measure') && mesure[1] !== '' && mesure[1] !== null
+        )).map((mesure) => mesure[1]);
+      setMesures(mesuresList as string[]);
     } else {
       const recipeData = await fetchDrinkId(id as string);
       setDrinkRecipe(recipeData);
@@ -32,31 +40,80 @@ function RecipeInProgess() {
           .includes('Ingredient') && ingredient[1] !== '' && ingredient[1] !== null
         )).map((ingredient) => ingredient[1]);
       setIngredients(ingredientsList as string[]);
+      const mesuresList = Object.entries(recipeData)
+        .filter((mesure) => (mesure[0]
+          .includes('Measure') && mesure[1] !== '' && mesure[1] !== null
+        )).map((mesure) => mesure[1]);
+      setMesures(mesuresList as string[]);
     }
+    setIsLoaded(false);
   };
+
   useEffect(() => {
     fetchRecipe();
-    console.log(pathname);
-  }, [id]);
+  }, [pathname]);
 
-  const handleCheckboxChange = (index: any) => {
-    const updatedCheckedIngredients = {
-      ...done,
-      [index]: !done[index],
-    };
-    setDone(updatedCheckedIngredients);
-    const areAllChecked = ingredients
-      ? ingredients
-        .every((ingredient, index) => updatedCheckedIngredients[index]) : false;
-    setAllChecked(areAllChecked);
+  const handleCheckboxChange = (position: number) => {
+    if (pathname.includes('meals')) {
+      const updatedCheckedIngredients = {
+        ...done,
+        [position]: !done[position],
+        meals: [id],
+        drinks: done.drinks,
+      };
+      setDone(updatedCheckedIngredients);
+      localStorage
+        .setItem('inProgressRecipes', JSON.stringify(updatedCheckedIngredients));
+      const areAllChecked = ingredients
+        ? ingredients
+          .every((recipe, index) => updatedCheckedIngredients[index]) : false;
+      setAllChecked(areAllChecked);
+    }
+    if (pathname.includes('drinks')) {
+      const updatedCheckedIngredients = {
+        ...done,
+        [position]: !done[position],
+        meals: done.meals,
+        drinks: [id],
+      };
+      setDone(updatedCheckedIngredients);
+      localStorage
+        .setItem('inProgressRecipes', JSON.stringify(updatedCheckedIngredients));
+      const areAllChecked = ingredients
+        ? ingredients
+          .every((recipe, index) => updatedCheckedIngredients[index]) : false;
+      setAllChecked(areAllChecked);
+    }
   };
 
+  useEffect(() => {
+    const inProgressRecipes = JSON.parse(localStorage
+      .getItem('inProgressRecipes') || '{}');
+    setDone(inProgressRecipes);
+  }, [pathname]);
+
   const handleClick = () => {
+    const recipesFinished = {
+      id: mealRecipe?.idMeal ? mealRecipe.idMeal : drinkRecipe?.idDrink,
+      type: mealRecipe?.idMeal ? 'meal' : 'drink',
+      nationality: mealRecipe?.strArea ? mealRecipe.strArea : '',
+      category: mealRecipe?.strCategory
+        ? mealRecipe.strCategory : drinkRecipe?.strCategory,
+      alcoholicOrNot: drinkRecipe?.strAlcoholic ? drinkRecipe.strAlcoholic : '',
+      name: mealRecipe?.strMeal ? mealRecipe.strMeal : drinkRecipe?.strDrink,
+      image: mealRecipe?.strMealThumb
+        ? mealRecipe.strMealThumb : drinkRecipe?.strDrinkThumb,
+      doneDate: new Date().toISOString(),
+      tags: mealRecipe?.strTags ? mealRecipe.strTags.split(',') : [],
+    };
+    setDoneRecipes([...doneRecipes, recipesFinished]);
+    localStorage
+      .setItem('doneRecipes', JSON.stringify([...doneRecipes, recipesFinished]));
     navigate('/done-recipes');
   };
 
   const handleShare = () => {
-    navigator.clipboard.writeText(`http://localhost:3000${pathname}`);
+    navigator.clipboard.writeText(`http://localhost:3000/${mealRecipe?.idMeal ? 'meals' : 'drinks'}/${mealRecipe?.idMeal ? `${mealRecipe.idMeal}` : `${drinkRecipe?.idDrink}`}`);
     const alert = document.createElement('div');
     alert.innerHTML = 'Link copied!';
     document.body.appendChild(alert);
@@ -65,6 +122,7 @@ function RecipeInProgess() {
     }, 2000);
   };
 
+  if (isLoaded) return <div>Loading...</div>;
   return (
     <>
       {pathname.includes('meals') && (
@@ -80,6 +138,7 @@ function RecipeInProgess() {
             {ingredients?.map((ingredient, index) => (
               <li key={ index }>
                 <label
+                  className={ done[index] ? styles.done : '' }
                   htmlFor={ ingredient }
                   data-testid={ `${index}-ingredient-step` }
                 >
@@ -88,9 +147,10 @@ function RecipeInProgess() {
                     name=""
                     id={ ingredient }
                     onChange={ () => handleCheckboxChange(index) }
+                    checked={ done[index] }
                   />
-                  <span className={ done[index] ? styles.done : '' }>
-                    {`${ingredient} - ${mealRecipe[`strMeasure${index + 1}`]}`}
+                  <span>
+                    {`${ingredient} - ${mesures?.[index]}`}
                   </span>
                 </label>
               </li>
@@ -108,6 +168,7 @@ function RecipeInProgess() {
             type="button"
             onClick={ handleClick }
             disabled={ !allChecked }
+            data-testid="finish-recipe-btn"
           >
             Finish recipe
           </button>
@@ -133,6 +194,7 @@ function RecipeInProgess() {
             {ingredients?.map((ingredient, index) => (
               <li key={ index }>
                 <label
+                  className={ done[index] ? styles.done : '' }
                   htmlFor={ ingredient }
                   data-testid={ `${index}-ingredient-step` }
                 >
@@ -141,9 +203,10 @@ function RecipeInProgess() {
                     name=""
                     id={ ingredient }
                     onChange={ () => handleCheckboxChange(index) }
+                    checked={ done[index] }
                   />
-                  <span className={ done[index] ? styles.done : '' }>
-                    {`${ingredient} - ${drinkRecipe[`strMeasure${index + 1}`]}`}
+                  <span>
+                    {`${ingredient} - ${mesures?.[index]}`}
                   </span>
                 </label>
               </li>
@@ -157,7 +220,14 @@ function RecipeInProgess() {
             Share
           </button>
           <FavoriteButton mealRecipe={ mealRecipe } drinkRecipe={ drinkRecipe } />
-          <button type="button" onClick={ handleClick }>Finish recipe</button>
+          <button
+            type="button"
+            onClick={ handleClick }
+            disabled={ !allChecked }
+            data-testid="finish-recipe-btn"
+          >
+            Finish recipe
+          </button>
           <p data-testid="recipe-category">{ drinkRecipe?.strAlcoholic }</p>
           <p data-testid="instructions">{ drinkRecipe?.strInstructions }</p>
         </div>
